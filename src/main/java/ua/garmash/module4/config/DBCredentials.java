aea26afff9d7dbe4b94bf8012b0e4cab7809891f
Pilulkin70 <gsv@ukr.net>
2022-07-19 15:47:46 +0300

    WIP update

--- src/main/java/ua/garmash/module4/config/DBCredentials.java
+++ src/main/java/ua/garmash/module4/config/DBCredentials.java
@@ -21,6 +21,7 @@ public class DBCredentials {
     public static DBCredentials getCredentials() {
         if (instance == null) {
             final String databaseUriEnv = System.getenv("DATABASE_URL");
+//            final String databaseUriEnv ="postgres://ehnthqyzrkxllk:0a255d3e5efff1ef873016adfd7497f4e9bcc40906aa9df11986ae6efb470412@ec2-54-246-185-161.eu-west-1.compute.amazonaws.com:5432/d6k8p174f4nvki";
             String dbUrl = "jdbc:postgresql://localhost:5432/postgres";
             String username = "postgres";
             String password = "root";
